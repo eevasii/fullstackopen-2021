72b1b6218ed8a1e907e5eb18a9e53dc81e2a2481
Eeva <eeva.siikaaho@gmail.com>
2021-01-11 22:22:42 +0200

    Done exercise 1.5 kurssitiedot step5

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom';
 const Header = (props) => {
   return (
     <div>
-      <p>{props.course}</p>
+      <p>{props.course.name}</p>
     </div>
   )
 }
 const Part = (props) => {
   return (
+    
     <div>
 
       {props.part.name} {props.part.exercises}
@@ -38,27 +39,30 @@ const Total = (props) => {
 
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+  
   
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
