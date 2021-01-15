553792700ccec1b77eefdf4e88930bd177f8644b
Eeva <eeva.siikaaho@gmail.com>
2021-01-15 16:24:45 +0200

    Done exercise 1.6: unicafe step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodFeedback = () => setGood(good + 1)
+  const badFeedback = () => setBad(bad + 1)
+  const neutralFeedback = () => setNeutral(neutral + 1)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      
+      <Button
+        handleClick={goodFeedback}
+        text='good'
+      />
+      <Button
+        handleClick={neutralFeedback}
+        text='neutral'
+      />     
+      <Button
+        handleClick={badFeedback}
+        text='bad'
+      />
+      <h2>statistics</h2>
+      
+      <div>Good {good}</div>
+      <div>Neutral {neutral}</div>
+      <div>Bad {bad}</div>
+
+
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
